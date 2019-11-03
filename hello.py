51ffcdd9c0f8f7db16cb56635875be5a9386176e
Pauline M. Qin <paulinem.qin@Paulines-Air.wv.cc.cmu.edu>
2019-11-03 11:56:16 -0500

    Updated hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 from flask import Flask
-
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
@@ -9,3 +8,9 @@ def hello_world():
 @app.route('/page2')
 def hello_world_2():
 	return 'Hello, world 2!'
+
+@app.route('/page3')
+def hello_world_3():
+	f = open("Home.html", "r")
+	contents = f.read
+	return contents
