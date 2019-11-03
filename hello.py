e03d0f900d7018b97da5627ca21ab2874e23a282
matthewmguo <mmguo@andrew.cmu.edu>
2019-11-03 16:32:26 -0500

    Updated hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 app = Flask(__name__)
 
 @app.route('/homepage')
@@ -10,3 +11,22 @@ def hello_world():
 @app.route('/page2')
 def hello_world_2():
 	return 'Hello, world 2!'	
+
+
+@app.route('/img/<filename>')
+def static_proxy_img(filename):
+	root_dir = os.path.dirname(os.getcwd())
+	serve_dir = os.path.join(root_dir, 'GreenMaps/img')
+	return send_from_directory(serve_dir, filename)
+
+@app.route('/css/<filename>')
+def static_proxy_css(filename):
+	root_dir = os.path.dirname(os.getcwd())
+	serve_dir = os.path.join(root_dir, 'GreenMaps/css')
+	return send_from_directory(serve_dir, filename)
+
+@app.route('/fonts/<filename>')
+def static_proxy_fonts(filename):
+	root_dir = os.path.dirname(os.getcwd())
+	serve_dir = os.path.join(root_dir, 'GreenMaps/fonts')
+	return send_from_directory(serve_dir, filename)
